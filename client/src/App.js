1ebc35219f369f25e314640ec38e2a09676e0f66
Ana <morgi.chin@gmail.com>
2020-07-01 20:04:59 -0300

    implementing the last week table

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          <AppNavbar />
+          {/* <AppNavbar /> */}
           <Container>
             <LoadUser />
             {/* <EntryList /> */}
