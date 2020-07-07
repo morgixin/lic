d225dd71447dc327d3cadad3c20bc9ee95b2d5a0
Ana <morgi.chin@gmail.com>
2020-07-07 20:39:57 -0300

    Add main entry to home page

--- client/src/store.js
+++ client/src/store.js
@@ -6,19 +6,12 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeEnhancers(applyMiddleware(...middleware))
-// );
-
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
