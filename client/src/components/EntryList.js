50f789ee0522ab66502673d5934d13330adc62a9
Ana <morgi.chin@gmail.com>
2020-07-07 22:29:42 -0300

    make it better

--- client/src/components/EntryList.js
+++ client/src/components/EntryList.js
@@ -27,7 +27,7 @@ class Entries extends Component {
           : 0;
       })
       // .filter((entry) => entry.hora_leitura != Date.now())
-      .slice(entries.length - 5, entries.length)
+      .slice(entries.length - 6, entries.length - 1)
       .map((entry) => (
         <tr key={entry._id}>
           <td>
